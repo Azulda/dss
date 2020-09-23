8248a251e806624e7ab8fdfcddc46a9da539798c
Azulda <azuldacian@gmail.com>
2020-09-23 16:57:54 -0300

    Updates

---
+++ precache-manifest.e008d4eb27446a82b8cdf97c44f04aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ee4ef2681290edc48b1882f5c1bb24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "160d49768f06176157f4",
+    "url": "./static/css/main.2424fbc9.chunk.css"
+  },
+  {
+    "revision": "998ed15a57d9fd5b27aa",
+    "url": "./static/js/2.91c0c1bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.91c0c1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160d49768f06176157f4",
+    "url": "./static/js/main.41c190d5.chunk.js"
+  },
+  {
+    "revision": "82c4fded3b3c6ec14083",
+    "url": "./static/js/runtime-main.01555689.js"
+  }
+]);
